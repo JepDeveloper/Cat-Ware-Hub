6bfabd59042b038d2fdd06c14375dc395f210f2b
iCyphy <72982537+iCyphyDev@users.noreply.github.com>
2021-05-28 20:06:19 +0530

    Create ahdautopunch.lua

---
+++ scripts/ahdautopunch.lua
@@ -0,0 +1,11 @@
+getgenv().P = true --type false here instead of true and execute to stop the auto punch
+;
+
+
+
+while wait() do
+    if getgenv().P == true then
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer("Punch", "Right")
+game:GetService("ReplicatedStorage").RemoteEvent:FireServer("Punch", "Left")
+end
+end
